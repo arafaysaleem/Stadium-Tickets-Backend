b4cebdffb73d5813d20eff5a15aee1addabcbeba
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-10-12 20:07:16 +0500

    fix route loader

--- src/loaders/routes.loader.js
+++ src/loaders/routes.loader.js
@@ -2,12 +2,12 @@
 const healthCheckRouter = require('../routes/healthCheck.routes');
 const authRouter = require('../routes/auth.routes');
 const userRouter = require('../routes/user.routes');
-const { ApiKeyAuth } = require('../middleware/auth.middleware');
+const { apiKeyAuth } = require('../middleware/auth.middleware');
 
 class RoutesLoader {
     static initRoutes(app, version) {
         // Check api key on all routes
-        app.use(`/api/${version}/*`, ApiKeyAuth());
+        app.use(`/api/${version}/*`, apiKeyAuth());
         
         app.use(`/api/${version}/health`, healthCheckRouter);
         app.use(`/api/${version}/auth`, authRouter);
