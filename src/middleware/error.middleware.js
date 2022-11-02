fdeeb96859a28343ddb042027d19205c8478441c
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-11-02 06:14:12 +0500

    refact(Middleware): add unhandled excpn handling

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,21 +1,15 @@
 const { Config } = require('../configs/config');
 const { failureResponse } = require('../utils/responses.utils');
 const { InternalServerException } = require('../utils/exceptions/api.exception');
-const { DuplicateEntryException, ForeignKeyViolationException } = require('../utils/exceptions/database.exception');
+const { DuplicateEntryException, ForeignKeyViolationException, DatabaseException } = require('../utils/exceptions/database.exception');
 
 function errorMiddleware(err, req, res, next) {
     if (err.name && err.name.startsWith('Sequelize')){
-        if (err.name === 'SequelizeUniqueConstraintError'){
-            err = new DuplicateEntryException(err.original.detail);
-        } else if (err.name === 'SequelizeForeignKeyConstraintError'){
-            err = new ForeignKeyViolationException(err.original.detail);
-        } else {
-            err = err = new InternalServerException(err.original.detail || 'Internal server error');
-        }
-    } else if (!err.message) {
-        if (!err.isOperational) err = new InternalServerException('Internal server error');
-    } else if (err.status === 500) {
-        if (!err.isOperational) err = new InternalServerException(err.message, err.data);
+        err = dbErrorHandler(err);
+    } else if (err.status === 500 && !err.isOperational) {
+        err = new InternalServerException(err.message, err.data);
+    } else if (!err.code || (!err.message && !err.isOperational)) {
+        err = new InternalServerException('Internal server error');
     }
 
     let { message, code, status, data, stack } = err;
@@ -28,7 +22,19 @@ function errorMiddleware(err, req, res, next) {
 
     const response = failureResponse(code, message, data);
 
-    res.status(status).send(response);
+    res.status(status || 500).send(response);
+}
+
+function dbErrorHandler(err){
+    let error;
+    if (err.name === 'SequelizeUniqueConstraintError'){
+        error = new DuplicateEntryException(err.original.detail);
+    } else if (err.name === 'SequelizeForeignKeyConstraintError'){
+        error = new ForeignKeyViolationException(err.original.detail);
+    } else {
+        error = new DatabaseException(err.original.detail || 'Something wrong with the database');
+    }
+    return error;
 }
 
 module.exports = errorMiddleware;
