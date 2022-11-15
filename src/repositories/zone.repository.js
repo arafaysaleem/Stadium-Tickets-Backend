bc92a8c4399fdde8252e230a1e55f63f08e27887
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-11-16 01:19:06 +0500

    refact(Zone): move seats into sub arrays

--- src/repositories/zone.repository.js
+++ src/repositories/zone.repository.js
@@ -7,6 +7,7 @@ const {
     CreateFailedException,
     UnexpectedException
 } = require('../utils/exceptions/database.exception');
+const { DisabledSeatType } = require('../utils/enums/disabledSeatType.enum');
 
 class ZoneRepository {
     findAll = async(filters = {}) => {
@@ -30,22 +31,30 @@ class ZoneRepository {
 
         const eventBookingsList = await DbContext.EventBookings.findAllForZone(id);
         
-        const zoneSeatsList = disabledSeatsList.map((m) => ({
-            seat_number: m.seat_number,
-            seat_row: m.seat_row,
-            type: m.type
-        }));
+        let blockedSeatsList = [];
+        let missingSeatsList = [];
+        for (var disabledSeat of disabledSeatsList) {
+            var seat = { seat_number: disabledSeat.seat_number, seat_row: disabledSeat.seat_row };
+            if (disabledSeat.type === DisabledSeatType.Blocked) blockedSeatsList.push(seat);
+            else missingSeatsList.push(seat);
+        }
 
+        let bookedSeatsList = [];
         for (var booking of eventBookingsList) {
             var bookingSeatsList = booking.booking_seats.map((m) => ({
                 seat_number: m.seat_number,
-                seat_row: m.seat_row,
-                type: 'booked'
+                seat_row: m.seat_row
             }));
-            zoneSeatsList.push(...bookingSeatsList);
+            bookedSeatsList.push(...bookingSeatsList);
         }
 
-        return successResponse(zoneSeatsList);
+        var zoneSeats = {
+            blocked: blockedSeatsList,
+            missing: missingSeatsList,
+            booked: bookedSeatsList
+        };
+
+        return successResponse(zoneSeats);
     };
     
     create = async(body) => {
