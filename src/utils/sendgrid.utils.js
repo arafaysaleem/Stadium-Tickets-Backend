bc7eab587c6bb14a975bcfed59f073173d820b28
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2023-01-27 05:03:08 +0500

    add snacks to sendgrid email data

--- src/utils/sendgrid.utils.js
+++ src/utils/sendgrid.utils.js
@@ -26,11 +26,12 @@ exports.sendOTPEmail = async(student, OTP) => {
 };
 
 exports.sendBookingSummaryEmail = async(
-    {order_date, order_amount, event, seats, parking, person},
+    {order_date, order_amount, event, seats, parking, snacks, person},
     id
 ) => {
     var currency = Config.CURRENCY;
     var parkingSummary = parking || {price: 'N/A', qty: 0, total: 0};
+    var snacksList = snacks || [];
     const msg = {
         to: person.email, // Change to your recipient
         from: Config.SENDGRID_SENDER, // Change to your verified sender
@@ -41,6 +42,7 @@ exports.sendBookingSummaryEmail = async(
             order_date: order_date, order_id: id, order_amount: `${currency}${order_amount}`,
             seat_price: seats.price, seat_qty: seats.qty, seat_total: `${currency}${seats.total}`,
             parking_price: parkingSummary.price, parking_qty: parkingSummary.qty, parking_total: `${currency}${parkingSummary.total}`,
+            snacks: snacksList,
             event_name: event.name, event_date: event.date, event_time: event.time
         }
     };
